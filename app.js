8c5a047a3fb1242c187da7bf7dec919e907d112e
Vitor Leonel <falecom@vitorleonel.com>
2020-09-12 01:43:34 -0300

    start project

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const Pusher = require("pusher");
+
+const app = express();
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: "us2",
+  useTLS: true,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  pusher.trigger(req.body.channel, "messages", req.body.data);
+
+  return res.status(204).json(null);
+});
+
+app.listen(4000, () => console.log("Server started."));
